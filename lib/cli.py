fe6dbd49a40512b10222711edc41bbba4c4d1d3b
Evan Lee <evan.lee.615@gmail.com>
2025-03-07 02:53:19 -0500

    fixed delete method in cli

--- lib/cli.py
+++ lib/cli.py
@@ -1,5 +1,7 @@
 # lib/cli.py
 
+from models.__init__ import CONN, CURSOR
+
 from models.user import (
     User
 )
@@ -34,50 +36,76 @@ def main():
         print("0. Exit the program")
         print("1. Functions for users")
         print("2. Functions for files")
-        main_choices = input(">>> ")
+        main_choice = input(">>> ")
 
-        if main_choices == "0":
+        if main_choice == "0":
             print("Goodbye!")
             exit()
 
-        elif main_choices == "1":
-            print("\033[4mFuntions for users\033[0m")
-            print("\033[4mPlease select an option\033[0m:")
-            print("0. Go back to previous menu")
-            print("1. Create a user")
-            print("2. Delete a user")
-            print("3. See all users")
-            print("***4. Find user by ID***")
-            print("5. See users by type")
-            print("6. Calculate total number of users")
-            print("7. Calculate number of users by type")
-            user_level_choices = input(">>> ")
+        elif main_choice == "1":
+            secondary_menu = "user choices"
+            while secondary_menu == "user choices":
+                print("\033[4mFuntions for users\033[0m")
+                print("\033[4mPlease select an option\033[0m:")
+                print("0. Go back to previous menu")
+                print("1. Create a user")
+                print("2. Delete a user")
+                print("3. See all users")
+                # print("***4. Find user by ID***")
+                print("4. See users by type")
+                print("5. Calculate total number of users")
+                print("6. Calculate number of users by type")
+                user_level_choice = input(">>> ")
 
-            User.create_table()
-                
-            if user_level_choices == "1":
-                print('\033[4mEnter username, then user type (without "")\033[0m:')
-                username = input(">>> ")
-                user_type = input(">>> ")
-                User.create(username, user_type)
-            elif user_level_choices == "2":
-                print("\033[4mEnter user's ID\033[0m:")
-                id = input(">>> ")
-                User.delete(id)
-            elif user_level_choices == "3":
-                User.get_all()
-            elif user_level_choices == "4":
-                User.find_by_id()
-            elif user_level_choices == "5":
-                User.users_by_type()
-            elif user_level_choices == "6":
-                User.number_users()
-            elif user_level_choices == "7":
-                User.number_users_by_type
-            else:              
-                print("Invalid choice")    
+                User.create_table()
+                    
+                if user_level_choice == "0":
+                    secondary_menu = ""
 
-        elif main_choices == "2":
+                elif user_level_choice == "1":
+                    print('\033[4mEnter username, then user type (without "")\033[0m:')
+                    username = input(">>> ")
+                    user_type = input(">>> ")
+                    User.create(username, user_type)
+                    print(f'User created! Username: {username}, User Type: {user_type}')
+
+                elif user_level_choice == "2":
+                    print('\033[4mEnter username (without "")\033[0m:')
+                    username = input(">>> ")
+
+                    def cli_delete(username):
+                        sql = """
+                            SELECT *
+                            FROM users
+                            WHERE username = ?
+                        """
+
+                        row = CURSOR.execute(sql, (username,)).fetchone()
+                        instance = User.instance_from_db(row) if row else None
+                        instance.delete()
+
+                    cli_delete(username)
+                    print(f'User deleted! Username: {username}')
+
+                elif user_level_choice == "3":
+                    User.get_all()
+
+                # elif user_level_choice == "4":
+                #     User.find_by_id()
+
+                elif user_level_choice == "4":
+                    User.users_by_type()
+
+                elif user_level_choice == "5":
+                    User.number_users()
+
+                elif user_level_choice == "6":
+                    User.number_users_by_type
+                    
+                else:              
+                    print("Invalid choice")    
+
+        elif main_choice == "2":
             print("Functions for files")
 
         else:
